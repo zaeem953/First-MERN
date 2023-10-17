5937825cf74731d0581230c1e820d2599d126f45
Intern1pk <Intern1pk@DESKTOP-QPK-03>
2023-10-17 11:27:45 +0500

    some Nav changes

--- front-end/src/components/Nav.jsx
+++ front-end/src/components/Nav.jsx
@@ -1,34 +1,32 @@
 import React, { useEffect } from 'react'
-import {Link, useNavigate} from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 function Nav() {
   // check if users data is in local storage
-  const auth=localStorage.getItem("user");
-  const navigate=useNavigate();
+  const auth = localStorage.getItem("user");
+  const navigate = useNavigate();
 
   // ----------------------LOGOUT FUNCTION----------------
-  const logout=()=>{
+  const logout = () => {
     localStorage.clear();
     navigate("/signup");
   }
-  
+
   return (
     <div>
-        <ul className='nav-ul'>
-            <li><Link to="/">Products</Link></li>
-            <li><Link to="/add">Add Products</Link></li>
-            <li><Link to="/update">Update Products</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            
-            {
-              auth ? <li><Link to="/signup" onClick={logout}>Logout</Link></li> 
-              :<> 
-              <li><Link to="/signup">Sign Up</Link></li>
-               <li><Link to="/login">Login</Link></li>
-               </>
-            }
-            
-        </ul>
+      {
+        auth ? <ul className='nav-ul'>
+          <li><Link to="/">Products</Link></li>
+          <li><Link to="/add">Add Products</Link></li>
+          <li><Link to="/update">Update Products</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+          <li><Link to="/signup" onClick={logout}>Logout ({JSON.parse(auth).name})</Link></li>
+        </ul> :
+          <ul className='nav-ul nav-right'>
+            <li><Link to="/signup">Sign Up</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+      }
     </div>
   )
 }
@@ -37,8 +35,8 @@ function Nav() {
 // <li>{auth ? <Link to="/signup" onClick={logout}>Logout</Link> :
 // <Link to="/signup">Sign Up</Link>}</li>
 // <li><Link to="/login">Login</Link></li>
-                
-                
-                
+
+
+
 
 export default Nav
